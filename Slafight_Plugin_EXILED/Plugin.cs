bd60c47bc0b9dbe93d27f707c6e2757d839dd52c
Slaviaaa2 <206885651+Slaviaaa2@users.noreply.github.com>
2026-05-12 22:20:53 +0900

    bump version

--- Slafight_Plugin_EXILED/Plugin.cs
+++ Slafight_Plugin_EXILED/Plugin.cs
@@ -34,7 +34,7 @@ public class Plugin : Plugin<Config>
     public override string Name => "Slafight_Plugin_EXILED";
     public override string Author => "Slaviaaa_2";
     public override string Prefix => "Slafight_Plugin_EXILED";
-    public override Version Version => new Version(1,7,5,6);
+    public override Version Version => new Version(1,8, 0, 0);
         
     public override Version RequiredExiledVersion { get; } = new Version(9, 13, 3);
 
