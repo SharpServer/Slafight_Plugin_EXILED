014a8aac41568077ed61eff5d72974933d6c3f5b
Slaviaaa2 <206885651+Slaviaaa2@users.noreply.github.com>
2026-05-23 12:11:15 +0900

    bafsghh

--- Slafight_Plugin_EXILED/CustomRoles/FoundationForces/LwsRoles.cs
+++ Slafight_Plugin_EXILED/CustomRoles/FoundationForces/LwsRoles.cs
@@ -10,7 +10,7 @@ namespace Slafight_Plugin_EXILED.CustomRoles.FoundationForces;
 
 public class LwsJudgement : CRole
 {
-    protected override string RoleName { get; set; } = "<color=#7ddcff>Law's Left Hand: Judgement</color>";
+    protected override string RoleName { get; set; } = $"<color={ServerColors.Silver}>Law's Left Hand: Judgement</color>";
     protected override string Description { get; set; } = "Omega-1を率いる裁定担当。\n財団の秩序を回復し、敵対勢力を排除せよ。";
     protected override CRoleTypeId CRoleTypeId { get; set; } = CRoleTypeId.LwsJudgement;
     protected override CTeam Team { get; set; } = CTeam.FoundationForces;
@@ -34,12 +34,12 @@ public class LwsJudgement : CRole
         [AmmoType.Nato9] = 80,
     };
     protected override UnityEngine.Vector3? SpawnPosition => MapFlags.FirstTeamSpawnPoint;
-    protected override string SpawnCustomInfo => $"<color={ServerColors.Nickel}>Law's Left Hand Judgement</color>";
+    protected override string SpawnCustomInfo => $"<color={ServerColors.Silver}>Law's Left Hand Judgement</color>";
 }
 
 public class LwsLiaison : CRole
 {
-    protected override string RoleName { get; set; } = "<color=#7ddcff>Law's Left Hand: Liaison</color>";
+    protected override string RoleName { get; set; } = $"<color={ServerColors.Silver}>Law's Left Hand: Liaison</color>";
     protected override string Description { get; set; } = "Omega-1の連絡官。\n現場の味方と連携し、重要人物の保護と敵対勢力の排除を遂行せよ。";
     protected override CRoleTypeId CRoleTypeId { get; set; } = CRoleTypeId.LwsLiaison;
     protected override CTeam Team { get; set; } = CTeam.FoundationForces;
@@ -61,12 +61,12 @@ public class LwsLiaison : CRole
         [AmmoType.Nato9] = 180,
     };
     protected override UnityEngine.Vector3? SpawnPosition => MapFlags.FirstTeamSpawnPoint;
-    protected override string SpawnCustomInfo => $"<color={ServerColors.Nickel}>Law's Left Hand Liaison</color>";
+    protected override string SpawnCustomInfo => $"<color={ServerColors.Silver}>Law's Left Hand Liaison</color>";
 }
 
 public class LwsForensic : CRole
 {
-    protected override string RoleName { get; set; } = "<color=#7ddcff>Law's Left Hand: Forensic</color>";
+    protected override string RoleName { get; set; } = $"<color={ServerColors.Silver}>Law's Left Hand: Forensic</color>";
     protected override string Description { get; set; } = "Omega-1の調査官。\n施設内の脅威を見極め、部隊の生存を支援せよ。";
     protected override CRoleTypeId CRoleTypeId { get; set; } = CRoleTypeId.LwsForensic;
     protected override CTeam Team { get; set; } = CTeam.FoundationForces;
@@ -89,12 +89,12 @@ public class LwsForensic : CRole
         [AmmoType.Nato9] = 120,
     };
     protected override UnityEngine.Vector3? SpawnPosition => MapFlags.FirstTeamSpawnPoint;
-    protected override string SpawnCustomInfo => $"<color={ServerColors.Nickel}>Law's Left Hand Forensic</color>";
+    protected override string SpawnCustomInfo => $"<color={ServerColors.Silver}>Law's Left Hand Forensic</color>";
 }
 
 public class LwsAgent : CRole
 {
-    protected override string RoleName { get; set; } = "<color=#7ddcff>Law's Left Hand: Agent</color>";
+    protected override string RoleName { get; set; } = $"<color={ServerColors.Silver}>Law's Left Hand: Agent</color>";
     protected override string Description { get; set; } = "Omega-1の実働員。\n命令に従い、施設の秩序回復を支援せよ。";
     protected override CRoleTypeId CRoleTypeId { get; set; } = CRoleTypeId.LwsAgent;
     protected override CTeam Team { get; set; } = CTeam.FoundationForces;
@@ -115,5 +115,5 @@ public class LwsAgent : CRole
         [AmmoType.Nato9] = 140,
     };
     protected override UnityEngine.Vector3? SpawnPosition => MapFlags.FirstTeamSpawnPoint;
-    protected override string SpawnCustomInfo => $"<color={ServerColors.Nickel}>Law's Left Hand Agent</color>";
+    protected override string SpawnCustomInfo => $"<color={ServerColors.Silver}>Law's Left Hand Agent</color>";
 }
