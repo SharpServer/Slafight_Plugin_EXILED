20ef4b883e67a7bc027b0f2fbaf8e912d1d28356
Slaviaaa2 <206885651+Slaviaaa2@users.noreply.github.com>
2026-05-23 12:55:40 +0900

    EXPERIMENTAL MOBILE TASK FORCES ADD UPDATE

--- Slafight_Plugin_EXILED/MainHandlers/RoundHandler.cs
+++ Slafight_Plugin_EXILED/MainHandlers/RoundHandler.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Exiled.API.Features;
 using MEC;
 using PlayerRoles;
 using Slafight_Plugin_EXILED.API.Enums;
 using Slafight_Plugin_EXILED.API.Interface;
 using Slafight_Plugin_EXILED.Extensions;
+using Random = UnityEngine.Random;
 
 namespace Slafight_Plugin_EXILED.MainHandlers;
 
@@ -24,12 +27,16 @@ public class RoundHandler : IBootstrapHandler
     {
         IsAlreadySpawned = false;
         WaitForSpawnTime = 240f;
+        lwsRandom = Random.Range(0, 6);
+        rrhRandom = Random.Range(0, 4);
         Timing.RunCoroutine(GuardsCoroutine());
     }
 
     public static float WaitForSpawnTime { get; private set; }
     public static float ElapsedTime { get; private set; }
     public static bool IsAlreadySpawned { get; private set; }
+    private static int lwsRandom;
+    private static int rrhRandom;
 
     public static bool IsSecurityTeamExpected()
     {
@@ -55,16 +62,79 @@ public class RoundHandler : IBootstrapHandler
         return chaosSideCount < foundationSideCount;
     }
 
+    public static bool IsRrhExpected()
+    {
+        var hasManager = Player.List.Any(p => p.GetCustomRole() is CRoleTypeId.FacilityManager);
+        if (!hasManager) return false;
+        var chaosSideCount = 0;
+        var foundationSideCount = 0;
+
+        foreach (var player in Player.List)
+        {
+            switch (player.GetTeam())
+            {
+                case CTeam.ChaosInsurgency:
+                case CTeam.ClassD:
+                    chaosSideCount++;
+                    break;
+                case CTeam.FoundationForces:
+                case CTeam.Scientists:
+                case CTeam.Guards:
+                    foundationSideCount++;
+                    break;
+            }
+        }
+
+        if (foundationSideCount+2 >= chaosSideCount) return false;
+        if (rrhRandom is 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    public static bool IsLwsExpected()
+    {
+        var chaosSideCount = 0;
+        var foundationSideCount = 0;
+
+        foreach (var player in Player.List)
+        {
+            switch (player.GetTeam())
+            {
+                case CTeam.ChaosInsurgency:
+                case CTeam.ClassD:
+                    chaosSideCount++;
+                    break;
+                case CTeam.FoundationForces:
+                case CTeam.Scientists:
+                case CTeam.Guards:
+                    foundationSideCount++;
+                    break;
+            }
+        }
+
+        if (foundationSideCount+2 >= chaosSideCount) return false;
+        if (lwsRandom is 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
     public static SpawnTypeId GetExpectedTeam()
     {
-        if (IsSecurityTeamExpected())
+        if (IsLwsExpected())
         {
-            return SpawnTypeId.SecurityTeam;
+            return SpawnTypeId.MtfLwsNormal;
         }
-        else
+
+        if (IsRrhExpected())
         {
-            return SpawnTypeId.ChaosAgents;
+            return SpawnTypeId.MtfRrhNormal;
         }
+
+        return IsSecurityTeamExpected() ? SpawnTypeId.SecurityTeam : SpawnTypeId.ChaosAgents;
     }
 
     private static IEnumerator<float> GuardsCoroutine()
@@ -77,16 +147,30 @@ public class RoundHandler : IBootstrapHandler
             if (ElapsedTime >= WaitForSpawnTime)
             {
                 Faction faction;
-                if (IsSecurityTeamExpected())
+                if (IsLwsExpected())
                 {
                     faction = Faction.FoundationStaff;
-                    SpawnSystem.ReplaceNextSpawn(SpawnTypeId.SecurityTeam);
+                    SpawnSystem.ReplaceNextSpawn(SpawnTypeId.MtfLwsNormal);
+                }
+                else if (IsRrhExpected())
+                {
+                    faction = Faction.FoundationStaff;
+                    SpawnSystem.ReplaceNextSpawn(SpawnTypeId.MtfRrhNormal);
                 }
                 else
                 {
-                    faction = Faction.FoundationEnemy;
-                    SpawnSystem.ReplaceNextSpawn(SpawnTypeId.ChaosAgents);
+                    if (IsSecurityTeamExpected())
+                    {
+                        faction = Faction.FoundationStaff;
+                        SpawnSystem.ReplaceNextSpawn(SpawnTypeId.SecurityTeam);
+                    }
+                    else
+                    {
+                        faction = Faction.FoundationEnemy;
+                        SpawnSystem.ReplaceNextSpawn(SpawnTypeId.ChaosAgents);
+                    }
                 }
+                Log.Debug($"[FIRST SPAWN]Next expected spawn: {SpawnSystem.PendingOverrideType}");
 
                 Respawn.GrantTokens(faction, 1);
                 Respawn.AdvanceTimer(faction, 999);
