241a7402a47eefde64a1e62bd5b0f070cd9a1513
Slaviaaa2 <206885651+Slaviaaa2@users.noreply.github.com>
2026-07-31 21:09:30 +0900

    LS-D5 Pill

--- Slafight_Plugin_EXILED/CustomRoles/Fifthist/FifthistPriest.cs
+++ Slafight_Plugin_EXILED/CustomRoles/Fifthist/FifthistPriest.cs
@@ -30,7 +30,7 @@ public class FifthistPriest : CRole
         ItemType.GunCOM18,
         ItemType.ArmorHeavy,
         typeof(KeycardFifthistPriest),
-        typeof(LsdPill),
+        typeof(LsdPillSafe),
         ItemType.SCP500,
         ItemType.Adrenaline,
         ItemType.GrenadeHE,
